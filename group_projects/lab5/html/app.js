be25f3776169430ba03ee341006f3e3e4906cdfe
Austin Tebbs <stinden1@yahoo.com>
2015-10-31 13:08:02 -0600

    Oct 31

--- group_projects/lab5/html/app.js
+++ group_projects/lab5/html/app.js
@@ -1,18 +1,32 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('MessageController', function($scope)
+myApp.controller('MessageController', function($scope, $http)
 {
 
   $scope.messagelist = [];
 
   $scope.onSend = function()
   {
-    $scope.messagelist.push({name: $scope.name, phoneNumber: $scope.phoneNumber, message: $scope.messageBody, done: false});
+    $scope.messagelist.push({name: $scope.name, phoneNumber: $scope.phoneNumber, message: $scope.messageBody, agent: $scope.agent, done: false});
     $scope.name = "";
     $scope.phoneNumber = "";
     $scope.messageBody = "";
     console.log("Sent!");
-    //console.log($scope.messagelist);
+    console.log("Agent: " + $scope.agent)
+    var url = "message";
+    var jobj = JSON.stringify($scope.messagelist);
+    $http({
+      method: "POST",
+      url: url,
+      data: jobj
+    }).then(function successCallback(data)
+    {
+      console.log("jobj: " + jobj);
+    }, function errorCallback(data)
+    {
+      console.log("error");
+      console.log(data);
+    });
   }
 
   $scope.removeMsg = function()
---
+++ group_projects/lab5/server.js
@@ -0,0 +1,67 @@
+var fs = require('fs');
+var http = require('http');
+var url = require('url');
+var readline = require('readline');
+var ROOT_DIR = "html/";
+
+http.createServer(function (req, res) {
+  var urlObj = url.parse(req.url, true, false);
+  // If this is our comments REST service
+  if(urlObj.pathname.indexOf("message") !=-1) {
+    console.log("message route");
+    if(req.method === "POST") {
+      console.log("POST message route");
+      //read form data
+      var jsonData = "";
+      req.on('data', function (chunk) {
+        jsonData += chunk;
+      });
+      req.on('end', function () {
+        var reqObj = JSON.parse(jsonData);
+        console.log(reqObj);
+        console.log("Name: " + reqObj.name);
+        console.log("Message: " + reqObj.message);
+
+        // Now put it into the database
+        var MongoClient = require('mongodb').MongoClient;
+        MongoClient.connect("mongodb://localhost/tbig", function(err, db) {
+          if(err) throw err;
+          db.collection('messages').insert(reqObj,function(err, records) {
+            console.log("Record added as "+records[0]._id);
+            res.writeHead(200);
+            res.end("");
+     });
+   });
+});
+    } else if (req.method === "GET") {
+      console.log("In Get");
+      // Read all of the database entries and return them in a JSON array
+      var MongoClient = require('mongodb').MongoClient;
+      MongoClient.connect("mongodb://localhost/tbig", function(err, db) {
+        if(err) throw err;
+        db.collection("comments", function(err, comments){
+          if(err) throw err;
+          comments.find(function(err, items){
+            items.toArray(function(err, itemArr){
+              console.log("Document Array: ");
+              console.log(itemArr);
+              res.writeHead(200);
+              res.end(JSON.stringify(itemArr));
+            });
+          });
+        });
+      });
+    }
+  } else {
+   // Normal static file
+    fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  }
+}).listen(8000);
