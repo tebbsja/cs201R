f920b5633d91b9b366e693b9e07f49a0071716fc
Austin Tebbs <stinden1@yahoo.com>
2015-11-02 23:11:15 -0700

    Nov 2

--- group_projects/lab5/html/app.js
+++ group_projects/lab5/html/app.js
@@ -4,6 +4,55 @@ myApp.controller('MessageController', function($scope, $http)
 {
 
   $scope.messagelist = [];
+  $scope.users = [];
+  console.log("agentx: " + $scope.agentx);
+  $scope.body = true;
+  $scope.login = false;
+
+  $scope.enter = function()
+  {
+    $scope.usrInitials = [];
+
+    $scope.users.push({user: $scope.initials});
+    console.log($scope.users);
+    var url = "user";
+    var jobj = JSON.stringify($scope.users);
+    $http({
+      method: "POST",
+      url: url,
+      data: jobj
+    }).then(function successCallback(data)
+      {
+        console.log("Success - user");
+        console.log("jobj: " + jobj);
+        console.log("data: " + data);
+        var url = "user";
+        $http({
+          method: "GET",
+          url: url
+        }).then(function successCallback(response)
+        {
+          console.log("succes");
+          console.log(response);
+          var items = response['data'];
+          for (var i=0; i < items.length; i++)
+          {
+              $scope.usrInitials.push({user: items[i].user});
+          }
+
+        }, function errorCallback(response)
+          {
+            console.log("error");
+            console.log(response);
+          });
+      }, function errorCallback(data)
+      {
+        console.log("error");
+        console.log(data);
+      });
+      $scope.body = false;
+      $scope.login = true;
+  }
 
   $scope.onSend = function()
   {
@@ -21,20 +70,78 @@ myApp.controller('MessageController', function($scope, $http)
       data: jobj
     }).then(function successCallback(data)
     {
-      console.log("jobj: " + jobj);
+      $scope.messagelist = [];
+
     }, function errorCallback(data)
     {
       console.log("error");
       console.log(data);
     });
+
+    $scope.refresh();
+  }
+
+  $scope.refresh = function()
+  {
+    $scope.specMessages = [];
+
+    $scope.agentx = $scope.initials;
+    console.log("agent: " + $scope.agentx);
+    var url = "message?q=" + $scope.agentx;
+    $http({
+      method: "GET",
+      url: url
+    }).then(function successCallback(response)
+    {
+      console.log("success");
+      console.log(response);
+
+      var items = response['data'];
+      for (var i=0; i < items.length; i++)
+      {
+        console.log(items[i].name + " " + items[i].phoneNumber + " " +items[i].message);
+          $scope.specMessages.push({name: items[i].name, phoneNumber: items[i].phoneNumber, message: items[i].message});
+      }
+
+    }, function errorCallback(response)
+    {
+      console.log("error");
+      console.log(response);
+    });
   }
 
   $scope.removeMsg = function()
   {
-    var oldmsgs = $scope.messagelist;
-    $scope.messagelist = [];
-    angular.forEach(oldmsgs, function(messages) {
-      if (!messages.done) $scope.messagelist.push(messages);
-    });
-  };
+    $scope.toDelete = [];
+    for (var i=0; i < $scope.specMessages.length; i++)
+    {
+      if ($scope.specMessages[i].done)
+      {
+        console.log("i: " + i + " " + $scope.specMessages[i].message);
+        $scope.toDelete.push({message: $scope.specMessages[i].message});
+      }
+    }
+    for (var i=0; i < $scope.toDelete.length; i++)
+    {
+      var url = "delete";
+      var jobj = JSON.stringify($scope.toDelete[i]);
+      console.log("SENDING TO DELETE: " + i + " " + jobj);
+      $http({
+        method: "POST",
+        url: url,
+        data: jobj
+      }).then(function successCallback(data)
+      {
+        console.log("success");
+
+      }, function errorCallback(data)
+      {
+        console.log("error");
+        console.log(data);
+      });
+    }
+    $scope.refresh();
+      
+  }
+
 });
--- group_projects/lab5/server.js
+++ group_projects/lab5/server.js
@@ -6,8 +6,44 @@ var ROOT_DIR = "html/";
 
 http.createServer(function (req, res) {
   var urlObj = url.parse(req.url, true, false);
-  // If this is our comments REST service
-  if(urlObj.pathname.indexOf("message") !=-1) {
+  var nUrlObj = url.format(urlObj);
+
+  if(urlObj.pathname.indexOf("delete") !=-1) {
+    console.log("DELETE ROUTE");
+    if(req.method === "POST") {
+      console.log("DELETE(post)ROUTE");
+      //read data;
+      var jsonData = "";
+      req.on('data', function (chunk) {
+        jsonData += chunk;
+      });
+      req.on('end', function() {
+        var reqObj = JSON.parse(jsonData);
+        //console.log("reqObj: " + reqObj[0]);
+        console.log(reqObj);
+        var count = 0;
+        var MongoClient = require('mongodb').MongoClient;
+        MongoClient.connect("mongodb://localhost/tbig", function(err, db) {
+          if(err) throw err;
+          db.collection("messages", function(err, messages){
+            if(err) throw err;
+            messages.find(reqObj, function(err, items){
+              items.toArray(function(err, itemArr){
+                console.log("Document Array: ");
+                console.log(itemArr);
+            messages.remove(reqObj, function(err, results) {
+              console.log("DELETED: " + results + " documents");
+                res.writeHead(200);
+                res.end("");
+              });
+              });
+            });
+          });
+        });
+      });
+    }
+  } // If this is our message REST service
+  else if(urlObj.pathname.indexOf("message") !=-1) {
     console.log("message route");
     if(req.method === "POST") {
       console.log("POST message route");
@@ -18,16 +54,77 @@ http.createServer(function (req, res) {
       });
       req.on('end', function () {
         var reqObj = JSON.parse(jsonData);
-        console.log(reqObj);
-        console.log("Name: " + reqObj.name);
-        console.log("Message: " + reqObj.message);
+        //console.log(reqObj);
+        //console.log("Name: " + reqObj.name);
+        //console.log("Message: " + reqObj.message);
+        //var toAdd = JSON.stringify(reqObj);
+        //console.log("toAdd: " + toAdd);
 
         // Now put it into the database
         var MongoClient = require('mongodb').MongoClient;
         MongoClient.connect("mongodb://localhost/tbig", function(err, db) {
           if(err) throw err;
           db.collection('messages').insert(reqObj,function(err, records) {
-            console.log("Record added as "+records[0]._id);
+            //console.log("Record added as " + records[0]._id);
+            res.writeHead(200);
+            res.end("");
+     });
+   });
+});
+    } else if (req.method === "GET") {
+      console.log("pathname: " + urlObj.pathname);
+      console.log("search: " + urlObj.search);
+      console.log("In Get");
+      var str = urlObj.search;
+      var agnt = str.slice("3");
+      console.log("agnt: " + agnt);
+      // Read all of the database entries and return them in a JSON array
+      var MongoClient = require('mongodb').MongoClient;
+      MongoClient.connect("mongodb://localhost/tbig", function(err, db) {
+        if(err) throw err;
+        db.collection("messages", function(err, messages){
+          if(err) throw err;
+          messages.find({agent: agnt}, function(err, items){
+            items.toArray(function(err, itemArr){
+              console.log("Document Array: ");
+              console.log(itemArr);
+              res.writeHead(200);
+              res.end(JSON.stringify(itemArr));
+            });
+          });
+        });
+      });
+    }
+  } else if (urlObj.pathname.indexOf("user") !=-1) {
+    console.log("in users");
+
+    if(req.method === "POST") {
+      console.log("POST message route");
+      //read form data
+      var jsonData = "";
+      req.on('data', function (chunk) {
+        jsonData += chunk;
+      });
+      req.on('end', function () {
+        var reqObj = JSON.parse(jsonData);
+        console.log(reqObj);
+
+        // Now put it into the database
+        var MongoClient = require('mongodb').MongoClient;
+        MongoClient.connect("mongodb://localhost/tbig", function(err, db) {
+          if(err) throw err;
+          db.collection('users').update(reqObj[0], reqObj[0], {upsert: true, w:1, forceServerObjectId: false}, function(err, data)
+          {
+              if (err)
+              {
+                console.log("error");
+                console.log(err);
+              }
+              else
+              {
+                console.log("success");
+              }//console.log("Record added as "+records[0]._id);
+
             res.writeHead(200);
             res.end("");
      });
@@ -39,9 +136,9 @@ http.createServer(function (req, res) {
       var MongoClient = require('mongodb').MongoClient;
       MongoClient.connect("mongodb://localhost/tbig", function(err, db) {
         if(err) throw err;
-        db.collection("comments", function(err, comments){
+        db.collection("users", function(err, users){
           if(err) throw err;
-          comments.find(function(err, items){
+          users.find(function(err, items){
             items.toArray(function(err, itemArr){
               console.log("Document Array: ");
               console.log(itemArr);
@@ -52,6 +149,7 @@ http.createServer(function (req, res) {
         });
       });
     }
+
   } else {
    // Normal static file
     fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
